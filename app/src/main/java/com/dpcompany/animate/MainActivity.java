2aeb3c1633b2610480deb10523d344b27f236193
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-13 15:00:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/dpcompany/animate/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dpcompany.animate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+
+        ImageView bart = (ImageView)findViewById(R.id.bart);
+        bart.animate().alpha(0f).setDuration(1000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
